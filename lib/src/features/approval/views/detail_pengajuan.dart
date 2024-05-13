f6a5cc1d95dacf0103bf48d848131a2adde7fb1f
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-05-13 21:11:39 +0700

    main: add line progress detail pengajuan

--- lib/src/features/approval/views/detail_pengajuan.dart
+++ lib/src/features/approval/views/detail_pengajuan.dart
@@ -92,93 +92,195 @@ class DetailPengajuanView extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Container(
-            width: Get.width,
-            decoration: BoxDecoration(
-              color: cWhite,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: cGrey_400,
-                  blurRadius: 15,
-                  offset: Offset(1, 1), // Shadow position
+          child: Column(
+            children: [
+              headerLineProses(),
+              spaceHeight(15),
+              Container(
+                width: Get.width,
+                decoration: BoxDecoration(
+                  color: cWhite,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: cGrey_400,
+                      blurRadius: 15,
+                      offset: Offset(1, 1), // Shadow position
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListView.builder(
-                    itemCount: data.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            spaceHeight(10),
-                            index == 6
-                                ? Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      spaceHeight(5),
-                                      Container(
-                                        width: Get.width,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                          color: cGrey_300,
-                                          border: Border.all(
-                                              color: cGrey_400, width: 1),
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(5),
-                                          child: Text(
-                                            "Shift Pengganti",
-                                            style: customTextStyle(
-                                                FontWeight.w700, 15, cPrimary),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListView.builder(
+                        itemCount: data.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                spaceHeight(10),
+                                index == 6
+                                    ? Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          spaceHeight(5),
+                                          Container(
+                                            width: Get.width,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                              color: cGrey_300,
+                                              border: Border.all(
+                                                  color: cGrey_400, width: 1),
+                                            ),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(5),
+                                              child: Text(
+                                                "Shift Pengganti",
+                                                style: customTextStyle(
+                                                    FontWeight.w700,
+                                                    15,
+                                                    cPrimary),
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                      ),
-                                      spaceHeight(10),
-                                    ],
+                                          spaceHeight(10),
+                                        ],
+                                      )
+                                    : Container(),
+                                Text(
+                                  data[index]['title'],
+                                  style: customTextStyle(
+                                      FontWeight.w500, 12, cGrey_700),
+                                ),
+                                spaceHeight(3),
+                                Text(
+                                  data[index]['value'],
+                                  style: customTextStyle(
+                                      FontWeight.w500, 14, cBlack),
+                                ),
+                                spaceHeight(7),
+                                if (index != data.length - 1)
+                                  Container(
+                                    width: Get.width,
+                                    height: 1,
+                                    color: cGrey_500,
                                   )
-                                : Container(),
-                            Text(
-                              data[index]['title'],
-                              style: customTextStyle(
-                                  FontWeight.w500, 12, cGrey_700),
+                                else
+                                  Container()
+                              ],
                             ),
-                            spaceHeight(3),
-                            Text(
-                              data[index]['value'],
-                              style:
-                                  customTextStyle(FontWeight.w500, 14, cBlack),
-                            ),
-                            spaceHeight(7),
-                            if (index != data.length - 1)
-                              Container(
-                                width: Get.width,
-                                height: 1,
-                                color: cGrey_500,
-                              )
-                            else
-                              Container()
-                          ],
-                        ),
-                      );
-                    },
-                  )
-                ],
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Column headerLineProses() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Pengajuan",
+              style: customTextStyle(FontWeight.w600, 12, cGrey_700),
+            ),
+            Text(
+              "On Progress",
+              style: customTextStyle(FontWeight.w600, 12, cGrey_700),
+            ),
+            Text(
+              "Selesai",
+              style: customTextStyle(FontWeight.w600, 12, cGrey_700),
+            ),
+          ],
+        ),
+        spaceHeight(5),
+        SizedBox(
+          width: Get.width,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: cPrimary_800,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.check,
+                          size: 14,
+                          color: cWhite,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        width: Get.width,
+                        height: 2,
+                        color: cGrey_400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: cGrey_400,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        width: Get.width,
+                        height: 2,
+                        color: cGrey_400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: cGrey_400,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
