67f3ccaf7cdd38f9e377e1eaac93b1e2fc4e7e80
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-05-17 22:44:33 +0700

    main: add function logout

--- lib/src/features/profile/controllers/profile_controller.dart
+++ lib/src/features/profile/controllers/profile_controller.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:presensi_gs/http/models/user_model.dart';
+import 'package:presensi_gs/routes/route_name.dart';
 import 'package:presensi_gs/utils/base_url.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -26,7 +27,6 @@ class ProfileController extends GetxController {
     };
     try {
       isLoading(true);
-      print(isLoading.value);
       if (token == null) {
         throw Exception("Token not found");
       }
@@ -37,7 +37,6 @@ class ProfileController extends GetxController {
       );
 
       final json = jsonDecode(response.body);
-      print(json);
       if (response.statusCode == 200) {
         userM = UserModel.fromJson(json);
       } else {
@@ -47,7 +46,38 @@ class ProfileController extends GetxController {
       print(e.toString());
     } finally {
       isLoading(false);
-      print(isLoading.value);
+    }
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    try {
+      if (token == null) {
+        throw Exception("Token not found");
+      }
+
+      http.Response response = await http.get(
+        Uri.parse("$base_url/logout"),
+        headers: headers,
+      );
+
+      if (response.statusCode == 200) {
+        Get.offAllNamed(RouteNames.login);
+        await prefs.remove('nama');
+        await prefs.remove('nip');
+        await prefs.remove('role');
+        await prefs.remove('token');
+        await prefs.clear();
+      } else {
+        debugPrint(response.body.toString());
+      }
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
