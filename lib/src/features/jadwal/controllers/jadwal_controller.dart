a5fa15a0f8bcbc1170480c4ef8955d7e191a363a
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-11-21 13:12:51 +0700

    main: fix jawal

--- lib/src/features/jadwal/controllers/jadwal_controller.dart
+++ lib/src/features/jadwal/controllers/jadwal_controller.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:presensi_gs/http/models/jadwal_model.dart';
 import 'package:presensi_gs/utils/base_url.dart';
 import 'package:presensi_gs/utils/constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,7 +9,6 @@ import 'package:http/http.dart' as http;
 
 class JadwalController extends GetxController {
   var isLoading = false.obs;
-  JadwalModel? jadwalM;
   var month = DateTime.now().getMonthNumber();
   var year = DateTime.now().getYear();
   var isEmptyData = false.obs;
@@ -25,7 +23,6 @@ class JadwalController extends GetxController {
   Future<void> getJadwal(month, year) async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
-    final nip = prefs.getString('nip');
     var headers = {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $token',
@@ -52,7 +49,6 @@ class JadwalController extends GetxController {
       } else {
         debugPrint("Terjadi kesalahan get data");
       }
-      print(json);
     } catch (e) {
       print(e.toString());
     } finally {
