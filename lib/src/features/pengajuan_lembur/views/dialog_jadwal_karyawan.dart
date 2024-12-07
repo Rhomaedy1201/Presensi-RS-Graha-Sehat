d0c469bb51cbce96fb460c4e66bbfcfa8d87e847
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-12-07 17:34:09 +0700

    main:  fix dialog jadwal karyawan on lembur

--- lib/src/features/pengajuan_lembur/views/dialog_jadwal_karyawan.dart
+++ lib/src/features/pengajuan_lembur/views/dialog_jadwal_karyawan.dart
@@ -6,49 +6,97 @@ import 'package:presensi_gs/utils/components/space.dart';
 
 Future<void> dialogJadwalKaryawan() {
   return Get.dialog(
-    Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 300),
-      child: Container(
-        decoration: BoxDecoration(
-          color: cWhite,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Container(
-              width: Get.width,
-              height: 50,
-              decoration: const BoxDecoration(
-                  color: cPrimary,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10))),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CustomText(
-                        text: "Jadwal Karyawan".tr,
-                        color: cBlack,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400),
-                    IconButton(
-                      onPressed: () {
-                        Get.back();
-                      },
-                      icon: const Icon(Icons.close, size: 20),
-                    )
-                  ],
+    Material(
+      type: MaterialType.transparency,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 300),
+        child: Container(
+          decoration: BoxDecoration(
+            color: cWhite,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Container(
+                width: Get.width,
+                height: 50,
+                decoration: const BoxDecoration(
+                    color: cPrimary,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10))),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomText(
+                          text: "Jadwal Karyawan".tr,
+                          color: cBlack,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400),
+                      IconButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        icon: const Icon(Icons.close, size: 20),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            spaceHeight(10),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-            // )
-          ],
+              spaceHeight(10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                  scrollDirection:
+                      Axis.horizontal, // Membolehkan scroll horizontal
+                  child: SingleChildScrollView(
+                    scrollDirection:
+                        Axis.vertical, // Membolehkan scroll vertikal
+                    child: DataTable(
+                      dataTextStyle: const TextStyle(
+                          fontSize: 11, fontWeight: FontWeight.w500),
+                      dataRowColor: const MaterialStatePropertyAll(cGrey_300),
+                      headingRowColor: MaterialStatePropertyAll(cPrimary),
+                      headingTextStyle: const TextStyle(
+                          fontSize: 11, fontWeight: FontWeight.w400),
+                      headingRowHeight: 30,
+                      dataRowHeight: 35,
+                      columns: const [
+                        DataColumn(label: Text('No')),
+                        DataColumn(label: Text('Unit')),
+                        DataColumn(label: Text('Nama')),
+                        DataColumn(label: Text('Tanggal')),
+                        DataColumn(label: Text('Shift')),
+                        DataColumn(label: Text('Absen')),
+                        DataColumn(label: Text('Pulang')),
+                        DataColumn(label: Text('Ket')),
+                        DataColumn(label: Text('Status')),
+                      ],
+                      rows: List<DataRow>.generate(
+                        1,
+                        (index) => DataRow(
+                          cells: [
+                            DataCell(Text('${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Muhammad RHomaedi ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                            DataCell(Text('Unit ${index + 1}')),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
